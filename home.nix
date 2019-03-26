b842ba50c75ff54b55580c09788e20b56898b6d9
Manuel Mendez <mmendez534@gmail.com>
2019-03-26 01:30:01 -0400

    commit current state

--- home.nix
+++ home.nix
@@ -1,41 +1,77 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.config = { allowUnfree = true; };
+
   home.packages = with pkgs; [
     alacritty
+    colordiff
     direnv
     docker_compose
     elvish
+    entr
     firefox-beta-bin
     font-manager
     freetype
+    fzf
     gettext
+    gimp
+    git-crypt
     git-lfs
+    gitAndTools.diff-so-fancy
+    gitAndTools.git-absorb
+    gitAndTools.gitstatus
+    gitFull
+    gitg
+    gnumake
     gnupg
+    go
     gotools
-    gtk3
+    grim
     gron
+    gtk3
     hack-font
     htop
+    htop
+    i3blocks
     inetutils
     inkscape
+    ipcalc
     ipmitool
     jo
+    jq
+    keybase
+    libarchive
     libjpeg_turbo
+    lshw
+    mako
+    meld
     mosh
     mpv
     mtr
     neovim
     nix-update-source
-    pwgen
-    python3Packages.python-language-server
+    pasystray
+    pavucontrol
+    pstree
     python3Packages.bpython
+    python3Packages.python-language-server
+    ripgrep
+    rofi
+    rsync
+    shellcheck
     socat
-    sway-beta
+    sway
+    swayidle
+    swaylock
     transmission-gtk
-    wget
+    tree
     vlc
+    #waybar
+    wget
     xclip
+    xwayland
+    #zoom-us
   ];
 
   services.gpg-agent = {
