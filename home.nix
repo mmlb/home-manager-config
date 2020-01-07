b1d645360275d9fa8752a999516b6793c678c217
Manuel Mendez <mmendez534@gmail.com>
2020-01-06 23:06:05 -0500

    Re-enable lorri daemon

--- home.nix
+++ home.nix
@@ -133,31 +133,31 @@ in {
   };
   services.pasystray.enable = true;
   services.syncthing.enable = true;
-  #
-  #  systemd.user.sockets.lorri = {
-  #    Unit = { Description = "lorri build daemon"; };
-  #    Socket = { ListenStream = "%t/lorri/daemon.socket"; };
-  #    Install = { WantedBy = [ "sockets.target" ]; };
-  #  };
-  #
-  #  systemd.user.services.lorri = {
-  #    Unit = {
-  #      Description = "lorri build daemon";
-  #      Documentation = "https://github.com/target/lorri";
-  #      ConditionUser = "!@system";
-  #      Requires = "lorri.socket";
-  #      After = "lorri.socket";
-  #      RefuseManualStart = true;
-  #    };
-  #
-  #    Service = {
-  #      ExecStart = "${lorri}/bin/lorri daemon";
-  #      PrivateTmp = true;
-  #      ProtectSystem = "strict";
-  #      WorkingDirectory = "%h";
-  #      Restart = "on-failure";
-  #      Environment = "PATH=${path} RUST_BACKTRACE=1";
-  #    };
-  #  };
+
+  systemd.user.sockets.lorri = {
+    Unit = { Description = "lorri build daemon"; };
+    Socket = { ListenStream = "%t/lorri/daemon.socket"; };
+    Install = { WantedBy = [ "sockets.target" ]; };
+  };
+
+  systemd.user.services.lorri = {
+    Unit = {
+      Description = "lorri build daemon";
+      Documentation = "https://github.com/target/lorri";
+      ConditionUser = "!@system";
+      Requires = "lorri.socket";
+      After = "lorri.socket";
+      RefuseManualStart = true;
+    };
+
+    Service = {
+      ExecStart = "${lorri}/bin/lorri daemon";
+      PrivateTmp = true;
+      ProtectSystem = "strict";
+      WorkingDirectory = "%h";
+      Restart = "on-failure";
+      Environment = "PATH=${path} RUST_BACKTRACE=1";
+    };
+  };
   xdg.enable = true;
 }
