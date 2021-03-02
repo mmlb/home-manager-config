edbb34285c00d77b2913a46714ba6a6129484626
Manuel Mendez <mmendez534@gmail.com>
2021-03-02 16:53:11 -0500

    update
    github.com/tomszilagyi/zutty/compare/4c05587b5893bdffcfa34362a74b4f79c9c8cb38...1c0f99b31497d0f75ee7bbb46e67ae5c0f775fdd

--- zutty.nix
+++ zutty.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation rec {
   pname = "zutty";
-  version = "unstable-2021-02-13g${builtins.substring 0 9 src.rev}";
+  version = "unstable-2021-02-28g${builtins.substring 0 9 src.rev}";
   src = pkgs.fetchFromGitHub rec {
     owner = "tomszilagyi";
     repo = "${pname}";
-    rev = "4c05587b5893bdffcfa34362a74b4f79c9c8cb38";
-    sha256 = "0dz475df88hcza9qxjpgxz5vqnbacxm57kw5gc1l58mc6d3sfg10";
+    rev = "1c0f99b31497d0f75ee7bbb46e67ae5c0f775fdd";
+    sha256 = "1riw5k32ph217h0c5izj5lmq2mq0p623a6n6pvw1j8lxw79rj392";
   };
   nativeBuildInputs = with pkgs; [ pkgconfig python wafHook ];
   buildInputs = with pkgs; [ xorg.libXaw freetype libglvnd ];
