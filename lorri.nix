73e7d47c94aa20847f72db2203d10241b4e524c3
Manuel Mendez <mmendez534@gmail.com>
2021-06-28 20:36:35 -0400

    update
    github.com/nix-community/lorri/compare/dd45c1f0ebea90102a0fe9b5fcc701302e71f1f6...dfbf9b3d22474380ee5e096931dbf25b1c162d10

--- lorri.nix
+++ lorri.nix
@@ -2,7 +2,7 @@
 (import (pkgs.fetchFromGitHub {
   owner = "nix-community";
   repo = "lorri";
-  #branch@date: master@2021-05-26
-  rev = "dd45c1f0ebea90102a0fe9b5fcc701302e71f1f6";
-  sha256 = "0hqml8ml76k1s8v6a0rzcf9sns87b2xnqplxm45cgp2rjs4qi2iz";
+  #branch@date: master@2021-06-21
+  rev = "dfbf9b3d22474380ee5e096931dbf25b1c162d10";
+  sha256 = "0n3n0wq6s13yznljrqqsjvjxjk6lg9j4bkxpxl2l517s6012hvxs";
 }))
