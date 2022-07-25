e7c72004b0ce34f446dd8a92f09db77e259a1960
Manuel Mendez <git@i.m.mmlb.dev>
2022-07-25 17:28:17 -0400

    move gdlv and tcat into nixpkgs

--- home.nix
+++ home.nix
@@ -6,30 +6,6 @@ let
     "${config.home.homeDirectory}/.local/bin"
   ];
 
-  gdlv = pkgs.buildGoModule rec {
-    pname = "gdlv";
-    version = "1.7.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "aarzilli";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "1jr7y5q7az2bkz43r12snfdjk8jdn281ys8r716am2nvgqax0z44";
-    };
-    vendorSha256 = null;
-  };
-
-  tcat = pkgs.buildGoModule rec {
-    pname = "tcat";
-    version = "1.0.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "rsc";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "1szzfz5xsx9l8gjikfncgp86hydzpvsi0y5zvikd621xkp7g7l21";
-    };
-    vendorSha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
-  };
-
   goDevPackages = with pkgs; [
     asmfmt
     delve
